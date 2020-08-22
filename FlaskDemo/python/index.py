16b9eaaeb7fa1d4c4a75aacef3d88d55b44edbfc
Brandon Meier <bsm52@zips.uakron.edu>
2020-08-22 15:15:20 -0400

    added paragraph in homepage

--- FlaskDemo/python/index.py
+++ FlaskDemo/python/index.py
@@ -1,5 +1,5 @@
 import flask
-from flask import render_template
+from flask import render_template, request
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -11,5 +11,14 @@ def index():
 @app.route('/')
 def main():
     return render_template('index.html', title='Home')
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    if 'name' in request.args:
+        name = request.args['name']
+        return f"Thanks for stopping by, {name}"
+    else:
+        return "Please enter your name"
+
 
 app.run(debug=True)
